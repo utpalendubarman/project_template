568707df80dd436c8ba5398aec26e4f05949c32b
utpalendubarman <47846069+utpalendubarman@users.noreply.github.com>
2021-04-13 18:20:31 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<!--Index Page-->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<form action='form.php' id='form' method='post'>
+    <div id='output'></div>
+    <input type='text' id='input' name='name'>
+    <button type='submit'>Submit</button>
+</form>
+<script>
+$(document).ready(function(){
+
+})
+</script>
